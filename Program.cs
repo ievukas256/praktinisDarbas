b61187180d4e3bf21419c378c44a33ad070686be
Your Nameievukas256 <you@example.comsibailaite@gmail.com>
2022-09-14 19:44:29 +0300

    trecia

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@
    jei kitu krastiniu ilgiai yra 10 ir 15, rez i kintamaji
 4. suskaiciuoti uzduotyse 1-3 gautu kintamuju suma ir vidurki , isvesti rez kaip pavyzdi*/
 
+//1.
 double r = 6371;
 double area = 4 * Math.PI * r * r;
-Console.WriteLine($"Rutulio plotas: {area}km");
+Console.WriteLine($"Kintamasis 1: {area}km");
+
+//3.----izambines ilgis----
+int a1 = 10;
+int b1 = 15;
+var rez = Math.Sqrt((a1 * a1) + (b1 * b1));
+Console.WriteLine($"Kintamasis 3: {rez}");
