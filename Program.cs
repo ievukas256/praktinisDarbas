67e993e9d3483f621c04e367b45d05d8e9aad37d
Your Nameievukas256 <you@example.comsibailaite@gmail.com>
2022-09-14 18:45:57 +0300

    surasytos uzduotys

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*1. suskaiciuoti rutulio plota spindulys 6371km, rezult rasyti i kintamaji
+2. suskaiciuoti turi 6371km, rez i kintamaji
+3. suskaiciuoti staciojo trikampio izambines(pries statuji kampa esanti staciojo trikampio krastine) ilgi 
+   jei kitu krastiniu ilgiai yra 10 ir 15, rez i kintamaji
+4. suskaiciuoti uzduotyse 1-3 gautu kintamuju suma ir vidurki , isvesti rez kaip pavyzdi*/
